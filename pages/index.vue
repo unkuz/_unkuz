004912b1f2724b209e66ca14c44e49c8a56dc174
crunkuz <crunkuzz@gmail.com>
2023-10-29 20:50:50 +0700

    🙄

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="w-screen h-screen flex justify-center items-center font-au text-[3rem]"
+  >
+    HOME
+  </div>
+</template>
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./layouts/**/*.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+    "./app/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        cuz: "Cuz",
+        au: "August",
+      },
+      keyframes: {
+        loading: {
+          "0%": { left: "-100%", width: "100%" },
+          "100%": { left: "100%", width: "100%" },
+        },
+      },
+      animation: {
+        loading: "loading 1s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
