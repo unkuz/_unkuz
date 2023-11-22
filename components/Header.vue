81ad52264a5df7174d710c8bb510c770ad9ca051
unkuz <hi.unkuz@gmail.com>
2023-11-23 00:48:07 +0700

    🍆

--- components/Header.vue
+++ components/Header.vue
@@ -10,13 +10,13 @@ console.log("Rout", route);
     <div title="Unkuz" class="flex items-center gap-[10px] font-au">
       <NuxtLink to="/">
         <UnkuzLogo
-          class="!fill-transparent [&>path]:!fill-transparent !w-[40px] !h-[40px] hover:cursor-pointer hover:rotate-[180deg] duration-300"
+          class="!fill-transparent [&>path]:!fill-transparent !w-[30px] !h-[30px] hover:cursor-pointer hover:rotate-[180deg] duration-300"
         />
       </NuxtLink>
     </div>
     <div class="flex gap-[20px] [&>div]:hover:cursor-pointer">
       <NuxtLink to="/work">
-        <div>_WORK</div>
+        <div class="">_WORK</div>
       </NuxtLink>
       <NuxtLink to="/about">
         <div>_ABOUT</div>
