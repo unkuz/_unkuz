949c7787f1bf21f53775a9b8721e524b40df4ecc
unkuz <hi.unkuz@gmail.com>
2023-11-23 22:02:00 +0700

    🌵

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-svgo", "nuxt-lodash"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-svgo", "nuxt-lodash", "@nuxt/image"],
   tailwindcss: {
     cssPath: "~/scss/tw.scss",
   },
--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,25 @@
 <template>
-  <div></div>
+  <div class="w-full flex justify-center flex-col items-center pt-[100px]">
+    <h1 class="text-[1rem]">About</h1>
+    <!-- <NuxtImg src="/unkuz.jpeg" class="w-[200px]" /> -->
+
+    <div class="indent-8">
+      A passionate FrontEnd developer with 2+ years of experience working in
+      building website. Proficient in HTML, CSS, JavaScript, TypeScript: plus
+      modern libraries and frameworks. Well-versed in requirements aith an
+      in-depth knowledge. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Illum amet consectetur hic magnam modi provident. Perferendis
+      deserunt soluta quod numquam modi itaque, quas autem provident
+      perspiciatis iure enim. Exercitationem, illo. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Sed impedit laudantium eveniet maxime
+      mollitia atque officiis sapiente consequatur vel, facilis ex tempore, quos
+      error adipisci dolores provident aut, voluptatem dolor.
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import Unkuz from "@/assets/image/unkuz.jpeg";
 useHead({
   title: "Unkuz | About Page",
 });
