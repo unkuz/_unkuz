08a58a7275559ca8f3165df291d9d7a09bc9f501
unkuz <hi.unkuz@gmail.com>
2023-11-24 00:11:50 +0700

    🥕

--- pages/work.vue
+++ pages/work.vue
@@ -1,5 +1,23 @@
 <template>
-  <div></div>
+  <div
+    class="w-full flex justify-center flex-col items-center pt-[100px] gap-[20px]"
+  >
+    <div>Side Project</div>
+    <div class="grid grid-cols-2 w-full gap-[20px]">
+      <div
+        class="w-full h-[200px] bg-slate-300 [clip-path:polygon(0_0,calc(100%-10px)_0,100%_10px,100%_100%,10px_100%,0_calc(100%-10px))]"
+      ></div>
+      <div
+        class="w-full h-[200px] bg-slate-300 [clip-path:polygon(0_0,calc(100%-10px)_0,100%_10px,100%_100%,10px_100%,0_calc(100%-10px))]"
+      ></div>
+      <div
+        class="w-full h-[200px] bg-slate-300 [clip-path:polygon(0_0,calc(100%-10px)_0,100%_10px,100%_100%,10px_100%,0_calc(100%-10px))]"
+      ></div>
+      <div
+        class="w-full h-[200px] bg-slate-300 [clip-path:polygon(0_0,calc(100%-10px)_0,100%_10px,100%_100%,10px_100%,0_calc(100%-10px))]"
+      ></div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
