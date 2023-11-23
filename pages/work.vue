4c09de7f077c386f9e89727cc9f7856954702da1
unkuz <hi.unkuz@gmail.com>
2023-11-24 01:52:23 +0700

    🙄

--- pages/work.vue
+++ pages/work.vue
@@ -3,7 +3,7 @@
     class="w-full flex justify-center flex-col items-center pt-[100px] gap-[20px]"
   >
     <div>Side Project</div>
-    <div class="grid sm:grid-cols-2 grid-cols-1 w-full gap-[20px]">
+    <div class="grid grid-cols-1 w-full gap-[20px]">
       <div
         class="w-full h-[200px] bg-slate-300 [clip-path:polygon(0_0,calc(100%-10px)_0,100%_10px,100%_100%,10px_100%,0_calc(100%-10px))]"
       ></div>
