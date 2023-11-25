1ef9857895fc30690398f68151ccbca023e895cb
unkuz <hi.unkuz@gmail.com>
2023-11-25 16:11:20 +0700

    🙄

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,11 @@
 <template>
-  <div
-    class="w-full flex justify-center flex-col items-center py-[100px] gap-[20px] min-h-screen"
-  >
+  <div class="w-full flex justify-center flex-col items-center py-[100px] gap-[20px] min-h-screen">
     <h1 class="text-[1rem]">About</h1>
-    <div
-      class="w-[350px] overflow-hidden"
-    >
-      <NuxtImg
-        src="/unkuz.jpeg"
-        class="object-cover overflow-hidden"
-      />
+    <div class="w-[350px] overflow-hidden">
+      <NuxtImg src="/unkuz.jpeg" class="object-cover overflow-hidden" />
     </div>
 
-    <div >
+    <div>
       <p class="indent-8 text-justify">
         Unkuz is the code name of Nguyen Ba Cuong, a creative web developer with
         2.5+ years of experience working in building website. Proficient in
@@ -22,41 +15,86 @@
         Hanoi as a web developer, while also taking on freelance projects that
         pique my interest. Here
         is the list of technologies that I have experience with:
-        
+
       <div class="[&_svg]:cursor-pointer indent-0 flex flex-col gap-[8px] mt-[10px]">
-        <span >Application and Data</span>
-        <div class="flex  gap-x-[20px] gap-y-[5px] flex-wrap [&>svg]:text-[30px]">
-          <Tailwind filled />
-          <ReactIcon filled  />
-          <NuxtIcon filled  />
-          <NextIcon filled class="!text-[45px]" />
-          <VueIcon filled />
-          <Lodash filled />
-          <Express filled />
-          <Nestjs filled />
-          <Django filled  class="!text-[45px]" />
-          <Nodejs filled  class="!text-[45px]" />
-          <Socketio filled />
-          <Python filled />
-          <Mongodb filled />
-          <Mysql filled />
-          <Postgresql filled />
+        <span>Application and Data</span>
+        <div class="flex  gap-x-[20px] gap-y-[5px] flex-wrap [&_svg]:text-[30px]">
+          <NuxtLink to="https://tailwindcss.com/" target="_blank">
+            <Tailwind filled />
+          </NuxtLink>
+          <NuxtLink  to="https://react.dev/"   target="_blank">
+            <ReactIcon filled  target="_blank"/>
+          </NuxtLink>
+          <NuxtLink  to="https://nuxt.com/"   target="_blank">
+            <NuxtIcon filled   />
+          </NuxtLink>
+          <NuxtLink to="https://nextjs.org/ " target="_blank">
+            <NextIcon filled class="!text-[45px]" />
+          </NuxtLink>
+          <NuxtLink to="https://vuejs.org/" target="_blank">
+            <VueIcon filled />
+          </NuxtLink>
+          <NuxtLink to="https://lodash.com/" target="_blank">
+            <Lodash filled />
+          </NuxtLink>
+          <NuxtLink to="https://expressjs.com/" target="_blank">
+            <Express filled />
+          </NuxtLink>
+          <NuxtLink to="https://nestjs.com/" target="_blank">
+            <Nestjs filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.djangoproject.com/" target="_blank">
+            <Django filled class="!text-[45px]" />
+          </NuxtLink>
+          <NuxtLink to="https://nodejs.org/en/" target="_blank">
+            <Nodejs filled class="!text-[45px]" />
+          </NuxtLink>
+          <NuxtLink to="https://socket.io/" target="_blank">
+            <Socketio filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.python.org/" target="_blank">
+            <Python filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.mongodb.com/" target="_blank">
+            <Mongodb filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.mysql.com/" target="_blank">
+            <Mysql filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.postgresql.org/" target="_blank">
+            <Postgresql filled />
+          </NuxtLink>
+
         </div>
-        <span >Utilities</span>
-        <div class="flex gap-x-[20px] gap-y-[8px] flex-wrap [&>svg]:text-[30px]">
-          <Framerjs filled />
-          <Jest filled />
-          <Figma filled />
+        <span>Utilities</span>
+        <div class="flex gap-x-[20px] gap-y-[8px] flex-wrap [&_svg]:text-[30px]">
+          <NuxtLink to="https://www.framer.com/" target="_blank">
+            <Framerjs filled />
+          </NuxtLink>
+          <NuxtLink to="https://jestjs.io/" target="_blank">
+            <Jest filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.figma.com/" target="_blank">
+            <Figma filled />
+          </NuxtLink>
         </div>
-        <span >DevOps</span>
-        <div class="flex gap-x-[20px] gap-y-[8px] flex-wrap [&>svg]:text-[30px]">
-          <Docker filled />
-          <Datadog filled  class="!text-[45px]" s/>
-          <Google_analytics filled />
-          <TravisCI filled />
+        <span>DevOps</span>
+        <div class="flex gap-x-[20px] gap-y-[8px] flex-wrap [&_svg]:text-[30px]">
+          <NuxtLink to="https://www.docker.com/" target="_blank">
+            <Docker filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.datadoghq.com/" target="_blank">
+            <Datadog filled class="!text-[45px]" s />
+          </NuxtLink>
+          <NuxtLink to="https://analytics.google.com/" target="_blank">
+            <Google_analytics filled />
+          </NuxtLink>
+          <NuxtLink to="https://www.travis-ci.com/" target="_blank">
+            <TravisCI filled />
+          </NuxtLink>
         </div>
       </div>
-      <div></div>
+      <!-- <div>On the other hand, he also a music producer</div> -->
       </p>
     </div>
   </div>
