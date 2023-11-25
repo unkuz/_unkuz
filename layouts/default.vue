a4507ace5d9189d7c57fccee39de6f27a82c4c47
unkuz <hi.unkuz@gmail.com>
2023-11-25 12:04:50 +0700

    😂

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,24 @@
-<template>
-  <div class="w-screen h-screen relative text-[.9rem] overflow-x-hidden">
-    <div ref="ani" class="fixed left-0 top-0 translate-x-[150%] z-[60]">
-      <Ani class="!w-auto !h-screen" />
-    </div>
-    <div
-      ref="sc1"
-      class="!w-screen !h-screen flex justify-center items-center font-au text-[2.5rem] tracking-widest select-none"
-    >
-      UNKUZ
-    </div>
-    <div ref="sc2" class="w-full mx-auto px-[20px] md:px-0 hidden">
-      <Header />
-      <div class="mx-auto sm:w-[700px]">
-        <slot />
-      </div>
-    </div>
-  </div>
-</template>
 <script lang="ts" setup>
 import Header from "@/components/Header.vue";
-
 import { gsap } from "gsap";
 import Ani from "~/svgs/ani.svg";
+
+const { isMobile } = useDevice();
 
 const ani = ref(null);
 const sc1 = ref(null);
 const sc2 = ref(null);
 
 onMounted(() => {
+  if (isMobile) {
+    gsap.set(sc1.value, {
+      display: "none",
+    });
+    gsap.set(sc2.value, {
+      display: "block",
+    });
+    return;
+  }
   const tl = gsap.timeline({});
   const animation = () =>
     tl
@@ -62,3 +53,23 @@ onMounted(() => {
   }, 1000);
 });
 </script>
+
+<template>
+  <div class="w-screen h-screen relative text-[.9rem] overflow-x-hidden">
+    <div ref="ani" class="fixed left-0 top-0 translate-x-[150%] z-[60]">
+      <Ani class="!w-auto !h-screen" />
+    </div>
+    <div
+      ref="sc1"
+      class="!w-screen !h-screen flex justify-center items-center font-au text-[2.5rem] tracking-widest select-none"
+    >
+      UNKUZ
+    </div>
+    <div ref="sc2" class="w-full mx-auto px-[20px] md:px-0 hidden">
+      <Header />
+      <div class="mx-auto sm:w-[700px]">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@vite-pwa/nuxt",
     "@vueuse/nuxt",
+    "@nuxtjs/device",
   ],
   tailwindcss: {
     cssPath: "~/scss/tw.scss",
