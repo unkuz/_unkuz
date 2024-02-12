a24c07ff105f8b1884cdfbdcac30b973fc296eb1
unkuz <hi.unkuz@gmail.com>
2024-02-12 22:53:49 +0700

    🥩

--- components/Miracle.vue
+++ components/Miracle.vue
@@ -1,54 +1,177 @@
 <template>
   <div
-    class="flex h-[60px] w-[calc(100vw-40px)] items-center gap-[10px] rounded-[10px] !bg-[#34d0ff] p-[5px] [box-shadow:3px_3px_10px_#0000007f] sm:h-[50px] sm:w-[400px]"
+    :class="[
+      'flex h-[60px] w-[calc(100vw-40px)] items-center rounded-[10px] !bg-[#34d0ff] p-[5px] [box-shadow:3px_3px_10px_#0000007f] sm:h-[50px] sm:w-[400px]',
+      {
+        'w-[60px] justify-center sm:w-[50px]': !isReady,
+      },
+    ]"
   >
     <div
-      class="aspect-square h-full overflow-hidden rounded-[7px] bg-fuchsia-200"
+      :class="[
+        'flex h-full w-full items-center gap-[10px]',
+        { 'hidden ': !isReady },
+      ]"
     >
-      <img
-        :src="state.thumbnail"
-        class="h-full w-full object-cover"
-        :alt="state.title"
-      />
-    </div>
-    <div class="flex flex-1 flex-col text-[.7rem] font-bold text-black">
-      <p class="line-clamp-2">{{ state.title }}</p>
-    </div>
-    <audio
-      ref="audioEl"
-      controls
-      class="hidden"
-      src="/Ball VRP & Novel Soul - Fake Colors (128 kbps).mp3"
-      :autoplay="false"
-    />
-    <canvas ref="canvasRef" class="h-[25px] w-[100px] sm:w-[120px]" />
-    <div class="mr-[5px] flex gap-[10px] *:cursor-pointer">
-      <div @click="togglePlay" class="[*>path]:fill-black *:w-[20px]">
-        <svg
-          v-if="!state.isPlay"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="black"
-            d="M10.396 18.433L17 12l-6.604-6.433A2 2 0 0 0 7 7v10a2 2 0 0 0 3.396 1.433"
-          />
-        </svg>
-        <svg
-          v-else
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="black"
-            d="M8 6a2 2 0 0 0-2 2v8a2 2 0 0 0 4 0V8a2 2 0 0 0-2-2m7 0a2 2 0 0 0-2 2v8a2 2 0 0 0 4 0V8a2 2 0 0 0-2-2"
-          />
-        </svg>
+      <div
+        class="aspect-square h-full overflow-hidden rounded-[7px] bg-fuchsia-200"
+      >
+        <img
+          :src="state.thumbnail"
+          class="h-full w-full object-cover"
+          :alt="state.title"
+        />
       </div>
+      <div class="flex flex-1 flex-col text-[.7rem] font-bold text-black">
+        <p class="line-clamp-2">{{ state.title }}</p>
+      </div>
+      <audio
+        ref="audioEl"
+        controls
+        class="hidden"
+        src="/Ball VRP & Novel Soul - Fake Colors (128 kbps).mp3"
+        :autoplay="false"
+      />
+      <canvas ref="canvasRef" class="h-[25px] w-[100px] sm:w-[120px]" />
+      <div class="mr-[5px] flex gap-[10px] *:cursor-pointer">
+        <div @click="togglePlay" class="[*>path]:fill-black *:w-[20px]">
+          <svg
+            v-if="!state.isPlay"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="black"
+              d="M10.396 18.433L17 12l-6.604-6.433A2 2 0 0 0 7 7v10a2 2 0 0 0 3.396 1.433"
+            />
+          </svg>
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="black"
+              d="M8 6a2 2 0 0 0-2 2v8a2 2 0 0 0 4 0V8a2 2 0 0 0-2-2m7 0a2 2 0 0 0-2 2v8a2 2 0 0 0 4 0V8a2 2 0 0 0-2-2"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div v-if="!isReady">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z"
+          transform="matrix(0 0 0 0 12 12)"
+        >
+          <animateTransform
+            id="svgSpinnersPulseRings30"
+            attributeName="transform"
+            begin="0;svgSpinnersPulseRings32.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            type="translate"
+            values="12 12;0 0"
+          />
+          <animateTransform
+            additive="sum"
+            attributeName="transform"
+            begin="0;svgSpinnersPulseRings32.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            type="scale"
+            values="0;1"
+          />
+          <animate
+            attributeName="opacity"
+            begin="0;svgSpinnersPulseRings32.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="1;0"
+          />
+        </path>
+        <path
+          fill="currentColor"
+          d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z"
+          transform="matrix(0 0 0 0 12 12)"
+        >
+          <animateTransform
+            id="svgSpinnersPulseRings31"
+            attributeName="transform"
+            begin="svgSpinnersPulseRings30.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            type="translate"
+            values="12 12;0 0"
+          />
+          <animateTransform
+            additive="sum"
+            attributeName="transform"
+            begin="svgSpinnersPulseRings30.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            type="scale"
+            values="0;1"
+          />
+          <animate
+            attributeName="opacity"
+            begin="svgSpinnersPulseRings30.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="1;0"
+          />
+        </path>
+        <path
+          fill="currentColor"
+          d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z"
+          transform="matrix(0 0 0 0 12 12)"
+        >
+          <animateTransform
+            id="svgSpinnersPulseRings32"
+            attributeName="transform"
+            begin="svgSpinnersPulseRings30.begin+0.8s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            type="translate"
+            values="12 12;0 0"
+          />
+          <animateTransform
+            additive="sum"
+            attributeName="transform"
+            begin="svgSpinnersPulseRings30.begin+0.8s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            type="scale"
+            values="0;1"
+          />
+          <animate
+            attributeName="opacity"
+            begin="svgSpinnersPulseRings30.begin+0.8s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="1;0"
+          />
+        </path>
+      </svg>
     </div>
   </div>
 </template>
@@ -57,6 +180,8 @@
 import { onMounted } from 'vue';
 import axios from 'axios';
 import { useMagicKeys } from '@vueuse/core';
+
+const isReady = ref(false);
 
 const audioEl = ref<HTMLMediaElement>();
 const canvasRef = ref<HTMLCanvasElement>();
@@ -67,7 +192,9 @@ const { space } = useMagicKeys();
 
 watch(space, (v) => v && togglePlay());
 
-onMounted(() => (ctxCanvas.value = canvasRef.value!.getContext('2d')));
+onMounted(() => {
+  ctxCanvas.value = canvasRef.value!.getContext('2d');
+});
 
 const state = reactive({
   track: new Audio(),
@@ -152,6 +279,8 @@ onMounted(async () => {
     url: 'https://www.youtube.com/watch?v=oX4uaarflA4&list=RDMM&index=2',
   });
   const data = res.data;
+
+  isReady.value = true;
 
   state.track.src = '/Ball VRP & Novel Soul - Fake Colors (128 kbps).mp3';
   state.title = data.title;
