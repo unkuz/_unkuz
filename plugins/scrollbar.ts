abd982b54367c860992ca6c00af06b3e6ee7543c
unkuz <hi.unkuz@gmail.com>
2024-12-15 07:46:09 +0700

    🍒

--- plugins/scrollbar.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app';
-import CustomScrollbar from 'custom-vue-scrollbar';
-
-export default defineNuxtPlugin((nuxtApp: any) => {
-  nuxtApp.vueApp.component('CustomScrollbarZ', CustomScrollbar);
-});
